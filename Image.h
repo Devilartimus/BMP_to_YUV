53056a9e71033bd1041ac7c3b754541b245ad86c
Artem Tikhonov <artemtihonov@list.ru>
2024-10-09 19:30:26 +0700

    Sample structure of the Image class

--- Image.h
+++ Image.h
@@ -1,8 +1,28 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+#include <string>
+#include <vector>
+
 class Image
 {
+public:
+
+    Image(const std::string& fileName);
+    void ConvertToYUV420();
+    const std::vector<unsigned char>& getYUVData() const;
+    int getWidth() const;
+    int getHeight();
+
+private:
+
+    std::vector<unsigned char> _RGB_DATA;
+    std::vector<unsigned char> _YUV_DATA;
+    int _WIDTH;
+    int _HEIGHT;
+
+    void loadBM(const std::string& fileName);
+    void RGBtoYUV();
 
 };
 
