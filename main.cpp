5b90170f53ff5b3be0451e62faa99ff27a0683f0
Artem Tikhonov <artemtihonov@list.ru>
2024-10-10 05:50:40 +0700

    Making prittier

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main()
 {
-    string videoName = "C:/Users/user/Desktop/Test/BMP_to_YUV/input.yuv";
-    string imageName = "C:/Users/user/Desktop/Test/BMP_to_YUV/BMP_352_288.bmp";
-    string outputName = "C:/Users/user/Desktop/Test/BMP_to_YUV/output.yuv";
+    string videoName = "C:/Users/user/Desktop/Test/BMP_to_YUV/input_and_output_files/input.yuv";
+    string imageName = "C:/Users/user/Desktop/Test/BMP_to_YUV/input_and_output_files/BMP_352_288.bmp";
+    string outputName = "C:/Users/user/Desktop/Test/BMP_to_YUV/input_and_output_files/output.yuv";
 
     Processor processor(videoName, imageName);
     processor.process(outputName);
